14e0f72da597457b86ae836f926bdd23b80d927e
dehemi weerakoon <115975647+dehemiweerakoon@users.noreply.github.com>
2025-02-10 22:17:34 +0530

    added student get request for single request

--- src/main/java/com/example/rolebased01/Controller/UserController.java
+++ src/main/java/com/example/rolebased01/Controller/UserController.java
@@ -2,6 +2,7 @@ package com.example.rolebased01.Controller;
 
 import com.example.rolebased01.Entity.CourseMarking;
 import com.example.rolebased01.Service.CourseMarkingService;
+import com.example.rolebased01.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,16 +16,25 @@ import java.util.List;
 public class UserController {
 
     @Autowired
+    private StudentService studentService;
+
+    @Autowired
     private CourseMarkingService courseMarkingService;
 
 
     @GetMapping("/marks/{id}")
-    public ResponseEntity<?> getMarks(@PathVariable long id) {
-        try{
+    public ResponseEntity<List<CourseMarking>> getMarks(@PathVariable long id) throws Exception {
+
             List<CourseMarking> courseMarking = courseMarkingService.getByStudentId(id);
             return ResponseEntity.status(HttpStatus.OK).body(courseMarking);
+
+    }
+    @GetMapping("/student/{id}")
+    public ResponseEntity<?> getStudent(@PathVariable long id) {
+        try {
+            return  ResponseEntity.status(HttpStatus.OK).body(studentService.getStudent(id));
         } catch (Exception e) {
-           return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 }
